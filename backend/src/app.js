676e47ea9e3b0d2c9e8de0bc5a76e81fb0b5e916
Andy Malak <amalak@albany.edu>
2025-11-12 16:15:10 +0000

    added /api/time that gives the current UTC time

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,10 @@ app.get("/health", (req, res) => {
   res.status(200).send("healthy");
 });
 
+app.get("/api/time", (req, res) => {
+  res.json({"timezone": "UTC", "currentTime": new Date().toLocaleTimeString()});
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
